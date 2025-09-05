7e67efaef45b3023d87fa550d8586a1a069444c5
Dipika Sonkusre <dipika@weetechsolution.com>
2025-09-05 10:52:54 +0530

    feat: Basic Routes with action and loader

---
+++ src/component/Layout.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+import { Outlet } from "react-router";
+import Footer from "./Footer";
+
+export default function Layout() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
